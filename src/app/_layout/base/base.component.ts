72586e1036f812f31c5ef4793f83dc19ab5b9620
Jitan Gupta <gtangupta@gmail.com>
2020-07-01 17:19:58 +0530

    changed absolute path to relative path

--- src/app/_layout/base/base.component.ts
+++ src/app/_layout/base/base.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { environment } from 'src/environments/environment';
-import { AuthService } from 'src/app/services/auth.service';
+import { environment } from '../../../environments/environment';
+import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
 
 @Component({
